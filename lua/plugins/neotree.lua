b7a84c22d46acba395e5b017138ef6c8a64ac4b1
Ramses Angles <ramses.angles@skiff.com>
2024-02-09 09:34:14 -0600

    added none-ls

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,4 +1,4 @@
-return 
+return
      {
         "nvim-neo-tree/neo-tree.nvim",
             branch = "v3.x",
@@ -8,7 +8,7 @@ return
                 "MunifTanjim/nui.nvim",
             },
             config = function()
-               vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+               vim.keymap.set('n', '<C-n>', ':Neotree toggle <CR>', {})
         end
     }
 
