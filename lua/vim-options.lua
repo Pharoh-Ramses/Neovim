5d9d653185e6abb41467c0fdbc9e0e398a35ebde
Ramses Angles <ramses.angles@hey.com>
2024-04-04 10:24:53 -0500

    added tmux control

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,3 +3,12 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
+
+--Navigate vim panels betters
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+--Navigate between buffers
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
